d15055e74ade068544872257780ac6949b87aff7
João Valença <jsvalenca@gmail.com>
2016-12-02 00:45:26 +0000

    sort of works with arguments

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
  
 from subprocess import *
+from sys import argv 
+from math import floor,ceil
 
-d = (1600,900) #size of my monitor
-o = (10,81)    #coords measured at the top left of the screen
+d = (1600,900)  #size of my monitor
+o = (  10, 80)  #coords measured at the top left of the screen
+
+GEO = {
+        "video_top"   :[(68, 0,32,33),(80, 0,20,21),(50, 0,50,50)],
+        "video_centre":[(10,10,80,80),(25,25,50,51),(33,33,32,33)]
+}[argv[1]]
 
 def runProcess(exe):
     p = Popen(exe.split(" "), stdout=PIPE, stderr=STDOUT)
@@ -24,7 +31,7 @@ def relativeP(x):
     return (x[0]-o[0])*100/d[0],(x[1]-o[1])*100/d[1]
 
 def absoluteP(x):
-    return (x[0]*d[0]/100)+o[0],(x[1]*d[1]/100)-o[1]
+    return (x[0]*d[0]/100)+o[0],(x[1]*d[1]/100)+o[1]
                                                         
 def relativeG(x):
     return x[0]*100/d[0],x[1]*100/d[1]
@@ -33,10 +40,6 @@ def absoluteG(x):
    return x[0]*d[0]/100,x[1]*d[1]/100
         
 if __name__ == "__main__":
-
-    GEOS = [(68,0,32,33),(80,0,20,21),(50,0,50,50)]
- 
-    d = (1600,900-15)
  
     w = call("xdotool getwindowfocus")
     l = call("xdotool getwindowgeometry %s" % w)
@@ -48,22 +51,22 @@ if __name__ == "__main__":
  
     k=p+g
  
-    k=tuple(map(round,k))
+    k=tuple(map(floor,k))
  
     print(k)
 
-    if k in GEOS:
+    if k in GEO:
          print("gravity found!")
-         k = GEOS[(GEOS.index(k)+1) % len(GEOS)]
+         k = GEO[(GEO.index(k)+1) % len(GEO)]
     else:
         print("gravity not found :(")
-        k = GEOS[0]
+        k = GEO[0]
 
     p = absoluteP(k[:2])
     g = absoluteG(k[2:])
  
-    l = call("xdotool windowsize %d %d %d" % (w,g[0],g[1]))
-    l = call("xdotool windowmove %d %d %d" % (w,p[0],p[1]))
-    l = call("xdotool mousemove %d %d" % (-o[0]+p[0]+g[0]/2, 27+o[1]+p[1]+g[1]/2))
+    l = call("xdotool windowsize %d %d%% %d%%" % (w,k[2],k[3]))
+    l = call("xdotool windowmove %d %d%% %d%%" % (w,k[0],k[1]))
+    l = call("xdotool mousemove %d %d" % (-o[0]+p[0]+g[0]//2, 25-o[1]+p[1]+g[1]//2))
 
 
