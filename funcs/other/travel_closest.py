2fcb84cb7d8fa3fddf4231d2b9bf97d0a3994ea1
João Valença <jsvalenca@gmail.com>
2016-12-04 23:37:33 +0000

    added cone priority to directional travel

--- funcs/other/travel_closest.py
+++ funcs/other/travel_closest.py
@@ -3,11 +3,11 @@ from itertools import groupby
 from funcs.util import *
 
 def travel(d):
-    d = {              #AVPos  Sign  Horiz
-            "left"  : (    1,  True,  True),
-            "right" : (    1, False,  True),
-            "up"    : (    2,  True, False),
-            "down"  : (    2, False, False)
+    d = {
+            "left"  : (1,  True),
+            "right" : (1, False),
+            "up"    : (2,  True),
+            "down"  : (2, False)
             }[d]
 
     n = call("xdotool get_desktop")
@@ -38,17 +38,13 @@ def travel(d):
     
     a.sort(key=itemgetter(3))
    
-    t=[]
     for i in range(len(a)):
         if abs(a[i][d[0]] - m[d[0]]) < 5: # Rounding threshold
             continue
         if abs(a[i][d[0]] - c[d[0]]) < 5:
             continue
         if (a[i][d[0]] > m[d[0]]) != d[1]:
-            if (abs(a[i][1]-m[1])>abs(a[i][2]-m[2]))==d[2]:
-                call("xdotool mousemove %d %d" % tuple(a[i][1:3]))
-                return
-            elif t==[]:
-                t=a[i]
-    if t!=[]:
-        call("xdotool mousemove %d %d" % tuple(t[1:3]))
+            call("xdotool mousemove %d %d" % tuple(a[i][1:3]))
+            break 
+
+
