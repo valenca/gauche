cedfe99e6a3549cc61ffcf937d9017b2a48cac9a
João Valença <jsvalenca@gmail.com>
2016-12-18 22:59:33 +0000

    fixed rounding error

--- funcs/gravity.py
+++ funcs/gravity.py
@@ -1,4 +1,3 @@
-from math import floor
 from time import sleep
 from operator import sub,add
 
@@ -42,8 +41,8 @@ def gravity(GEO):
         grav = GEO[0]
         print("gravity not found, defaulting to %s" % str(grav))
 
-    pos = absoluteP(grav[:2], display, offset)
-    geo = absoluteG(grav[2:], display, offset)
+    pos = tuple(map(round,absoluteP(grav[:2], display, offset)))
+    geo = tuple(map(round,absoluteG(grav[2:], display, offset)))
  
     last = call("wmctrl -i -r %d -e 0,%d,%d,%d,%d" % (window, pos[0], pos[1], geo[0], geo[1]))
     last = call("xdotool mousemove %d %d" % (pos[0] + geo[0] / 2, pos[1] + geo[1] / 2 ))
