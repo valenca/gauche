6d80f22bbc6d274bcd0049d1d8212445c9d59b22
João Valença <joao.valenca@feedzai.com>
2016-12-20 14:08:54 +0000

    wong name escape

--- funcs/stick.py
+++ funcs/stick.py
@@ -3,13 +3,12 @@ from re import escape
 
 def stick():
 	window = getCurrentWindow()
-	name = escape(call("xdotool getwindowname %s" % window))
+	name = call("xdotool getwindowname %s" % window)
 	if name[:3] == "[*]":
 		if DEBUG: print("Sticky window found: unsticking.")
-		call("wmctrl -i -r %s -N %s" % (window, name[3:]))
+		call("wmctrl -i -r %s -N %s" % (window, escape(name[3:])))
 		call("wmctrl -i -r %s -b remove,sticky" % window)
 	else:
 		if DEBUG: print("Non-Sticky window found: sticking.")
-		call("wmctrl -i -r %s -N %s" % (window, "[*]" + name))
+		call("wmctrl -i -r %s -N %s" % (window, "[*]" + escape(name)))
 		call("wmctrl -i -r %s -b add,sticky" % window)
-
