9b92caea07e94fe47b5af33db1b4a5ed841d056d
João Valença <jsvalenca@gmail.com>
2016-12-27 23:48:57 +0000

    changed chat gravity

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from funcs.util import *
 GEO = {
         "video_top"   :[(68,  0, 32, 33), (80,  0, 20, 21), (50,  0, 50, 50)],
         "video_centre":[(10, 10, 80, 80), (25, 25, 50, 51), (34, 34, 32, 32)],
-        "chat_bottom" :[(82, 55, 18, 45), (82, 93, 18,  7)],
+        "chat_bottom" :[(82, 55, 18, 45), (82, 96, 18,  4)],
 
         "left"        :[(0 ,  0,100,100), ( 0,  0, 50,100), ( 0,  0, 67,100), ( 0,  0, 33,100)],
         "right"       :[(50,  0, 50,100), (67,  0, 33,100), (33,  0, 67,100)],
@@ -33,18 +33,18 @@ if __name__ == "__main__":
             try:
                 gravity(GEO[argv[2]])
             except IndexError:
-                print("No gravity given. Must be one of these values:\n" + str(list(GEO.keys())))
+                print(     "No gravity given. Must be one of:\n" + str(list(GEO.keys())))
             except KeyError:
-                print("Gravity not supported. Must be one of these values:\n" + str(list(GEO.keys())))
+                print("Gravity not supported. Must be one of:\n" + str(list(GEO.keys())))
             quit()
 
         if argv[1] == "travel":
             try:
                 travel(argv[2])
             except IndexError:
-                print("No travel direction given. Must be one of these values:\n" + str(["up","down","left","right"]))
+                print(     "No travel direction given. Must be one of:\n" + str(["up","down","left","right"]))
             except KeyError:
-                print("Travel direction not supported. Must be one of these values:\n" + str(["up","down","left","right"]))
+                print("Travel direction not supported. Must be one of:\n" + str(["up","down","left","right"]))
             quit()
 
         if argv[1] == "split":
@@ -53,7 +53,7 @@ if __name__ == "__main__":
             except IndexError:
                 print("No direction given. Must be one of " + str(["horizontal","vertical"]) + ".")
             except KeyError:
-                print("Invalid argument. Must be one of " + str(["horizontal","vertical"]) + ".")
+                print(  "Invalid argument. Must be one of " + str(["horizontal","vertical"]) + ".")
             quit()
 
         if argv[1] == "stick":
@@ -62,6 +62,9 @@ if __name__ == "__main__":
 
         raise KeyError
     except IndexError:
-        print("No function given. Must be one of " + str(["gravity", "travel", "split", "stick"]))
+        print(     "No function given. Must be one of " + str(["gravity", "travel", "split", "stick"]))
     except KeyError:
         print("Function not supported. Must be one of " + str(["gravity", "travel", "split", "stick"]))
+    except Exception as e:
+        call("notify-send '%s'" % e)
+
